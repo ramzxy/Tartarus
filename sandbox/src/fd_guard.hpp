9845c055c8366773decc4154912ed36611d6ad96
ramzxy <ilia1000500095@gmail.com>
2026-02-06 01:54:39 +0100

    added folder structure and RAII

---
+++ sandbox/src/fd_guard.hpp
@@ -0,0 +1,27 @@
+class FileDescriptor {
+    public:
+    FileDescriptor(int fd) : fd_(fd) {
+        if (fd_ < 0) throw std::runtime_error("File descriptor creation failed");
+    }
+
+    ~FileDescriptor() {if (fd_ >= 0) close(fd_);}
+
+    //moveable and not copyable
+    FileDescriptor(FileDescriptor&& other) noexcept : fd_(other.fd_) {other.fd_ = -1;}
+    FileDescriptor& operator=(FileDescriptor&& other) noexcept {
+        if (this != &other) {
+            close(fd_);
+            fd_ = other.fd_;
+            other.fd_ = -1;
+        }
+        return *this;
+    }
+
+    FileDescriptor(const FileDescriptor&) = delete;
+    FileDescriptor& operator=(const FileDescriptor&) = delete;
+
+    int get() const { return fd_; }
+
+    private:
+    int fd_;
+}
